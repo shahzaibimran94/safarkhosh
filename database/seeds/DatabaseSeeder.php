e0b5af75f79e69ca0e0c2fd15c629ddd2ae49fea
shahzaibimran94 <shahzaib.imran94@gmail.com>
2017-04-24 22:29:46 -0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         DB::table('roles')->insert([
-            'name' => 'super',
+            'name' => 'admin',
         ]);
     }
 }
